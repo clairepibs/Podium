b3de6a47603ecb4e4e4ba4242f1e6eb60884f95d
clairepibs <46507543+clairepibs@users.noreply.github.com>
2019-03-16 22:20:21 -0700

    Update PODIUM functions.py

--- PODIUM functions.py
+++ PODIUM functions.py
@@ -18,6 +18,7 @@
 def stop(void):
 	'''
 	Stops motors immediately (no jarring control)
+	
 	INPUTS:     None
 	OUTPUTS:    None
 	'''
@@ -28,7 +29,7 @@ def stop(void):
 
 def step(motor, delay):
 	'''
-	One step
+	One step of motor at certain speed
 	
 	INPUTS:     motor to move, delay (s)
 	OUTPUTS:    None
@@ -42,38 +43,17 @@ def step(motor, delay):
 	GPIO.output(motor, LOW)
 	# delay for OFF
 	time.sleep(delay)
-
+	
 #-----------------------------------------------------------------------------
-
-
-def cleanup():
+def printLCD():
 	'''
-	Moves motors to (0,0), set speed to nominal, reset reference ('zero point') to (0,0).
-	INPUTS:    
+	Prints input input onto LCD screen. Uses global location variables
+	
+	INPUTS:
 	OUTPUTS:    
 	'''
-	
-	global x_loc_abs
-	global y_loc_abs
-	global x_loc
-	global y_loc
-	global zero_x
-	global zero_y
-	
-	# Send motors to neg limits
-	send_lim(lim_x_neg)
-	send_lim(lim_y_neg)
-    
-	# Set coordinates and zero to (0,0)
-	x_loc_abs = 0
-	y_loc_abs = 0
-	x_loc = 0
-	y_loc = 0
-	zero_x = 0
-	zero_y = 0
-
+	pass
 #-----------------------------------------------------------------------------
-    
 def send_lim(lim):
 	'''
 	Sends motor to given limit at nominal speed.
@@ -81,10 +61,10 @@ def send_lim(lim):
 	INPUTS:     limit switch to move to (pin number)
 	OUTPUTS:    
 	'''
-	
+	# Initialize global variables
 	global x_loc_abs
 	global y_loc_abs
-	
+	# Send to given limit switch
 	if lim == lim_x_pos:
 		motor = pul_x
 		GPIO.output(dir_x, right)
@@ -112,88 +92,93 @@ def send_lim(lim):
 		y_loc_abs = max_y
 	if lim == lim_y_neg:
 		y_loc_abs = 0
-
+		
+	# Set local coordinates and print location to LCD
+	x_loc = x_loc_abs - zero_x
+	y_loc = y_loc_abs - zero_y
+	printLCD()
 #-----------------------------------------------------------------------------
-
-def check_lims():
-    '''
-    Checks limits and stops appropriate motor if limits are hit. Indicates
-    limit has been hit on LCD and GUI.
-    
-    INPUTS:
-    OUTPUTS: Has limit been hit? (T/F)
-    '''
-    
-    # Might not need T/F outputs if just printing onto screens
-    
-	if GPTO.input(lim_x_pos) == True:
-		stop()
-		return posXtrig
-	if GPTO.input(lim_x_neg) == True:
-		stop()
-		return posYtrig
-	if GPTO.input(lim_y_pos) == True:
-		stop()
-		return negXtrig
-	if GPTO.input(lim_y_neg) == True:
-		stop()
-		return negYtrig
-	else:
-		return False
-
-#-----------------------------------------------------------------------------
-
-def set_zero():
+def cleanup():
 	'''
-	Sends motors to tared zero location
-	ENSURE YOU RESET LOCAL COORDINATES TO ZERO OUTSIDE FUNCTION
-    
-	INPUTS:     
+	Moves motors to (0,0), set speed to nominal, reset reference ('zero point') to (0,0).
+	INPUTS:    
 	OUTPUTS:    
 	'''
+	# Initialize global variables
+	global x_loc_abs
+	global y_loc_abs
+	global x_loc
+	global y_loc
 	global zero_x
 	global zero_y
 	
-	# Set new zero coordinates
-	zero_x = x_loc_abs
-	zero_y = y_loc_abs
-
+	# Send motors to neg limits
+	send_lim(lim_x_neg)
+	send_lim(lim_y_neg)
+    
+	# Set coordinates and zero to (0,0)
+	x_loc_abs = 0
+	y_loc_abs = 0
+	x_loc = 0
+	y_loc = 0
+	zero_x = 0
+	zero_y = 0
 #-----------------------------------------------------------------------------
-
 def accel_control():
-    '''
+	'''
 	Stops motors immediately (no jarring control)
 	INPUTS:     None
 	OUTPUTS:    None
-    '''
-    pass
-
+	'''
+	pass
 #-----------------------------------------------------------------------------
-
 def add_coord(void):
-    '''
-    Sends motors to tared zero location
-    
-    INPUTS:     
-    OUTPUTS:    
-    '''
-    pass
+	'''
+	Adds a coordinate to list on GUI, increases index
 
+	INPUTS:     
+	OUTPUTS:    
+	'''
+	global index
+	
+	sequence.append([x_loc_abs, y_loc_abs])
+	
+	index += 1
+	
+	printLCD()
+	
 #-----------------------------------------------------------------------------
-
 def send2zero():
-    '''
-    Sends motors to tared zero location
-    
-    INPUTS:     
-    OUTPUTS:    
-    '''
-    pass
+	'''
+	Sends motors to tared zero location
 
+	INPUTS:     
+	OUTPUTS:    
+	'''
+	pass
 #-----------------------------------------------------------------------------
 
 # PENDANT-SPECIFIC 
 
+#-----------------------------------------------------------------------------
+def check_lims_pend():
+    '''
+    Checks limits and stops appropriate motor if limits are hit.
+    
+    INPUTS:
+    OUTPUTS:
+    '''
+
+	if GPTO.input(lim_x_pos) == True:
+		stop()
+	if GPTO.input(lim_x_neg) == True:
+		stop()
+	if GPTO.input(lim_y_pos) == True:
+		stop()
+	if GPTO.input(lim_y_neg) == True:
+		stop()
+	else:
+		pass 
 #-----------------------------------------------------------------------------
 def pot_speed():
 	'''
@@ -210,8 +195,7 @@ def pot_speed():
 	delay_min = 0.00001
 	
 	# Read pot
-	pot_volt = chan.value
-	volt = convert_volt(pot_volt)
+	volt = chan.value
 	
 	# Conversion
 	volt_range = volt_max - volt_min
@@ -219,24 +203,122 @@ def pot_speed():
 	delay = abs(volt*delay_range/volt_range-delay_max)
 	
 	return delay
-
 #-----------------------------------------------------------------------------
-
-def printLCD():
-    '''
-    Prints input input onto LCD screen.
+def set_zero_pend():
+	'''
+	Sets current location to zero, updates location trackers
+	
+	INPUTS:     
+	OUTPUTS:    
+	'''
+	global zero_x
+	global zero_y
+	global x_loc
+	global y_loc
+	
+	# Set new zero coordinates
+	zero_x = x_loc_abs
+	zero_y = y_loc_abs
+	
+	# Reset local coordinates
+	x_loc = 0
+	y_loc = 0
+	
+	# Print new location on LCD
+	printLCD()
+#-----------------------------------------------------------------------------
+def pend_control():
+	'''
+	Reads buttons and potentiometer on pendant, moves motor
+	accordingly. Also saves locations based on step tracking.
     
-    INPUTS:     
-    OUTPUTS:    
-    '''
-    pass
-
+	INPUTS:
+	OUTPUTS:
+	'''
+	
+	# Initialize variables
+	global x_loc_abs
+	global y_loc_abs
+	global x_loc
+	global y_loc
+    
+	# Pop-up on GUI to avoid simultaneous use????????
+	
+	# Initialize button checks for zero and save
+	GPIO.add_event_detect(zero_but, GPIO.RISING, callback = set_zero_pend)
+	GPIO.add_event_detect(save_but, GPIO.RISING, callback = add_coord)
+	
+	# Setup motors and directions
+	if GPIO.input(right_but) == False:
+		GPIO.output(dir_x, right)
+		while GPIO.input(right_but) == False:
+			# Check if limits have been hit
+			check_lims_pend()
+			
+			delay = pot_speed()
+			step(pul_x, delay)
+		
+			# Increase location by 1
+			x_loc_abs += 1
+			x_loc += 1
+		
+			# Print location on LCD
+			printLCD()
+			
+	elif GPIO.input(left_but) == False:
+		GPIO.output(dir_x, left)
+		while GPIO.input(left_but) == False:
+			# Check if limits have been hit
+			check_lims_pend()
+			
+			delay = pot_speed()
+			step(pul_x, delay)
+		
+			# Increase location by 1
+			x_loc_abs -= 1
+			x_loc -= 1
+		
+			# Print location on LCD
+			printLCD()
+			
+	elif GPIO.input(up_but) == False:
+		GPIO.output(dir_y, right)
+		while GPIO.input(up_but) == False:
+			# Check if limits have been hit
+			check_lims_pend()
+			
+			delay = pot_speed()
+			step(pul_y, delay)
+		
+			# Increase location by 1
+			y_loc_abs += 1
+			y_loc += 1
+		
+			# Print location on LCD
+			printLCD()
+			
+	elif GPIO.input(down_but) == False:
+		GPIO.output(dir_y, right)
+		while GPIO.input(down_but) == False:
+			# Check if limits have been hit
+			check_lims_pend()
+			
+			delay = pot_speed()
+			step(pul_y, delay)
+		
+			# Increase location by 1
+			y_loc_abs -= 1
+			y_loc -= 1
+		
+			# Print location on LCD
+			printLCD()
+	
+	# Implement jarring control
 #-----------------------------------------------------------------------------
 
 # GUI SPECIFIC
 
 #-----------------------------------------------------------------------------
-
 def move_GUI(dest_x, dest_y):
     '''
     Sends motors to target location (LOCAL COORDINATES, NOT ABSOLUTE)
@@ -244,6 +326,77 @@ def move_GUI(dest_x, dest_y):
     INPUTS:     
     OUTPUTS:    
     '''
-    pass
+	# Initialize variables
+	global x_loc_abs
+	global y_loc_abs
+	global x_loc
+	global y_loc
+	delay = nom_speed
+	count = 0
+	
+	# Move to location
+	distance_x = dest_x - x_loc
+	distance_y = dest_y - y_loc
+	
+	# Move motors to new location and set new location variables
+	for count in range(0, distance_x):
+		step(pul_x, delay)
+		
+	x_loc_abs += distance_x
+	x_loc = x_loc_abs - zero_x
+	
+	for count in range(0, distance_y):
+		step(pul_y, delay)
+	y_loc_abs += distance_y
+	y_loc = y_loc_abs + distance_y
+	
+	# Print new location on LCD
+	printLCD()
+#-----------------------------------------------------------------------------
+def check_lims_GUI():
+    '''
+    Checks limits and stops appropriate motor if limits are hit. Indicates
+    limit has been hit on LCD and GUI.
+    
+    NOTE: DOES THIS ACTUALLY NEED TO CHECK PINS
+    
+    INPUTS:
+    OUTPUTS: Has limit been hit? (T/F)
+    '''
 
+	if GPTO.input(lim_x_pos) == True:
+		stop()
+		return posXtrig
+	if GPTO.input(lim_x_neg) == True:
+		stop()
+		return posYtrig
+	if GPTO.input(lim_y_pos) == True:
+		stop()
+		return negXtrig
+	if GPTO.input(lim_y_neg) == True:
+		stop()
+		return negYtrig
+	else:
+		return False
+#-----------------------------------------------------------------------------
+def set_zero_GUI(x,y):
+	'''
+	Sets current location to zero, updates location trackers
+	
+	INPUTS:		new zero coordinates (decimal)  
+	OUTPUTS:	
+	'''
+	global zero_x
+	global zero_y
+	
+	# Set new zero coordinates
+	zero_x = x
+	zero_y = y
+	
+	# Reset local coordinates
+	x_loc = x_loc_abs - zero_x
+	y_loc = y_loc_abs - zero_y
+	
+	# Print new location on LCD
+	printLCD()
 #-----------------------------------------------------------------------------
