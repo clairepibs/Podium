3ec28c85f816a215ed487e9af4eb9e3ebddb8a37
clairepibs <46507543+clairepibs@users.noreply.github.com>
2019-06-29 11:03:47 -0700

    Update config.py

--- config.py
+++ config.py
@@ -17,33 +17,36 @@ x_loc = 0
 # VARIABLES TO CHANGE FOR CALIBRATION
 
 # max x and y locations (mm)
-max_x = 135
-max_y = 135
+max_x = 133
+max_y = 87
 
 # Set a nominal speed (delay in secs)
-nom_speed = 0.001
-nom_accel = 0.001 # is max instantaneous change in speed
+nom_speed_x = 0.0005
+nom_speed_y = 0.0005
+nom_accel = 0.0005 # is max instantaneous change in speed
 
 # Define zeros
 zero_x = 0
 zero_y = 0
 
 # Define speed/delay and rotpot values
-volt_max = 65472
-volt_min = 2000
-delay_max = 0.01
-delay_min = 0.00005
+volt_max = 65474
+volt_min = 200
+delay_max = 0.001
+delay_min_x = 0.0004
+delay_min_y = 0.0004
 
 # define stepper motor directions
 UP = 1
 DOWN = 0
-RIGHT = 1
-LEFT = 0
+RIGHT = 0
+LEFT = 1
 
-# steps per mm (steps per rotation = 400, rotation = 1.27)
-steps_per_mm = int(400/1.27)
-mm_per_step = 1/steps_per_mm
+# steps per mm (steps per rotation = 400, rotation = 1.27 mm/rot)
+steps_per_mm = 315
+mm_per_step = round(1/steps_per_mm, 4)
 
 # flag to stop eveything
 stopflag = 0
+stop_lim_thread = 0
 #-----------------------------------------------------------------------------
