8d151f184bdf840f9f41db91650f211bdf1f5e9f
clairepibs <46507543+clairepibs@users.noreply.github.com>
2019-07-08 20:16:13 -0700

    Create podium_init.cmd

---
+++ podium_init.cmd
@@ -0,0 +1,7 @@
+:: This is the batch file to initialize the Raspberry Pi SSH connection and begin windowing for the PODIUM GUI.
+
+ @ECHO OFF
+ 
+ :: Open windows powershell, establish ssh connection with Pi
+ 
+ http://steve-jansen.github.io/guides/windows-batch-scripting/part-2-variables.html
